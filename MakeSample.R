85c6c08e2a4d54e1147614a52b8b851cb7a99bfd
Keith Miller <KeithDKMiller@kennis-research.com>
2016-01-18 22:02:12 -0500

    Fixes Issue #2 Removed verbosity from Web App Moves code for "phrase" function to Predictor file in Web App/test directory Deleted obsolete files from Web App folder

--- MakeSample.R
+++ MakeSample.R
@@ -11,9 +11,8 @@ library("plyr", lib.loc="~/R/win-library/3.2")
 library("git2r",      lib.loc="~/R/win-library/3.2")
 library("hash", lib.loc="~/R/win-library/3.2")
 
-
 setwd("~/R/Capstone")
-
+source("Web app/test/predictor.R")
 # Checks to see if original source files are loaded and loads them if required
 load.data<-function(){
   ### FILE INFO CALCUALTIONS
@@ -485,98 +484,7 @@ if (n==2) bigrams    <<-    bigrams [x %in% unigrams[,ngram]]
 
 
 # Predicts n possible next words from a phrase x 
-phrase <-function(target,n = 1,model = "Interpolate", params) {
-  
-  y <- ""
-  tags<-c("<p>","<n>", "<s>","<e>", "<UNK>")
-  
-  
-  target<-gsub   ('[<>]+'           ," ",target) #remove tagging chars
-  target<-tolower(target) #set everything to lowercase
-  target<-gsub   (profanity         ,"<p>", target) #tag profanity
-  target<-gsub   ('((([0-9]{1,3})(,[0-9]{3})*)|([0-9]+))(.[0-9]+)?',   "<n>",target) #tag numbers
-  target<-gsub   ('[\'’]'           , "" ,target)  #remove apostrophes
-  target<-gsub   ('[()\"“”:;,_-]'   , " ",target) #remove other characters
-  target<-gsub   ('[^a-zA-Z \n<>\']', " ",target) #remove anything that's not a letter
-  target<-gsub   ('[ ][ ]+'         , " ",target) #remove whitespace
-  
-  
-  
-  phrase.length <- stri_count_words(target) 
- 
-  if (phrase.length==1) target<-data.table(V1 = "", 
-                                           V2 = "", 
-                                           V3 = target) else 
-    if (phrase.length==2) target<-data.table(V1 = "", 
-                                             V2 = stri_extract_all_words(target)[[1]][1], 
-                                             V3 = stri_extract_all_words(target)[[1]][2]) else
-      target<-data.table(t(stri_extract_all_words(target)[[1]][(phrase.length-2):phrase.length]))
-  
-  #If phrase is longer than 3 words, discard all but the last 3
-  
 
-  
-  setnames(target, c("V1","V2", "V3"),c("u","v","w"))
-  
-  if (phrase.length>3) {phrase.length<-3}
-  
-  
-  # replace out of vocab words in target with <UNK> tag
-  target[,c("u","v","w"):=lapply(.SD , function(y) ifelse ((y %in% unigrams$x), y , "<UNK>"))]
-  
-  if (model %in% c("Backoff")) {
-        
-                 
-        # target<-data.table(u = target[1],v = target[2],w = target[3])
-     
-        if ( phrase.length  == 3 ) { y<-quadrigrams[target , .(x,probability), nomatch = 0 ]
-        
-                                  if (y[,.N]==0) phrase.length<-phrase.length-1 else y<-y[order(probability)[1:n],x]}#print("Backing off to trigrams")
-                                  
-        
-        if ( phrase.length  == 2 ) {y<-    trigrams[target[,.(v,w)] , .(x,probability)]
-
-
-
-                                  if (y[,.N]==0) phrase.length<-phrase.length-1 else y<-y[order(probability)[1:n], w]} #print("Backing off to bigrams")
-
-
-                                  
-        
-        if ( phrase.length  == 1 ) {y<-     bigrams[target[,w], .(x,probability)]
-       
-
-                                  if (y[,.N]==0) phrase.length<-phrase.length-1 else y<-y[order(probability)[1:n],v]} #print("Backing off to unigrams")
-
-
-        
-        if ( phrase.length  == 0 )  y<-    unigrams[order(probability),ngram ][1:n]
-
-  }
-  
-  else if (model=="Interpolate"){
-    
-  #create a table of probabilites of words based on for quadrigrams -> unigrams
-    table.predict<<-  data.table(rbind(
-
-
-   quadrigrams[target[,.(u,v,w)], nomatch = 0][!(x %in% tags)][,weighted.prob := params$l4* (probability/2^20)][order(-weighted.prob), .(x, weighted.prob,4)][1:min(.N,10)],   
-   trigrams   [target[,.(  v,w)], ][!(x %in% tags)][,weighted.prob := params$l3* 2^-probability][order(-weighted.prob), .(x, weighted.prob,3)][1:min(.N,10)], 
-   bigrams    [target[,.(    w)], ][!(x %in% tags)][,weighted.prob := params$l2* 2^-probability][order(-weighted.prob), .(x, weighted.prob,2)][1:min(.N,10)], 
-   unigrams   [1:100,]             [!(x %in% tags)][,weighted.prob := params$l1* 2^-probability][order(-weighted.prob), .(x, weighted.prob,1)]))
-    
-   ngram.probs<<- data.table()
-  
-   
-  #sum the probabilities      
-  y<-table.predict[, sum(weighted.prob, rm.na = T), by = x][order(-V1), x][1:n]
-  
-  
-    
-  }
-  
-  else print("Not a valid model")
-   y}
 
 #measures model accuracy against n test strings
 accuracy<-function(samp = 20, n = 500, model = "Interpolate", params){
--- Web app/test/app.R
+++ Web app/test/app.R
@@ -3,6 +3,8 @@ library(shinyBS)
 library(data.table)
 library(stringi)
 
+source("predictor.R")
+
 restore.ngrams<-function (path) {  
   unigrams<<- readRDS(file =  paste0(path,"unigrams.RDS"))
   bigrams<<-  readRDS(file =  paste0(path,"bigrams.RDS"))
@@ -67,11 +69,11 @@ server <- function(input, output, session) {
                      input$target
                      input$enabled
                      
-                     isolate(cat("Set timer: ", 
-                                 "input target:",input$target, 
-                                 "reac enabled:",reac$enabled, 
-                                 "reac target:",reac$target, 
-                                 "Predict:",reac$predict,"\n", sep =","))
+#                      isolate(cat("Set timer: ", 
+#                                  "input target:",input$target, 
+#                                  "reac enabled:",reac$enabled, 
+#                                  "reac target:",reac$target, 
+#                                  "Predict:",reac$predict,"\n", sep =","))
       
                      if (isolate(reac$predict))
                        {reac$target<-input$target
@@ -87,8 +89,9 @@ server <- function(input, output, session) {
    
   observe(  label = "Make Prediction",  
     
-                x = { isolate(cat("Make Prediction", input$target, input$enabled, reac$target, reac$predict, "\n", sep =","))
-                  #make sure reac has the latest version of input before predicting
+                x = { 
+                  # isolate(cat("Make Prediction", input$target, input$enabled, reac$target, reac$predict, "\n", sep =","))
+                  # make sure reac has the latest version of input before predicting
                      x <- if ( stri_endswith(reac$target, fixed = " ")) 
                             paste0(reac$target, if (reac$enabled) phrase(reac$target
                                                                           ,1,
@@ -103,88 +106,6 @@ server <- function(input, output, session) {
           })
   
 }
-phrase <-  
-  function(target,n = 1,model = "Interpolate", l1 = 0.2, l2 = 0.35, l3 = 0.45, l4= 0)
-    {
-  
-  y <- ""
- 
-  tags<-c("<p>","<n>", "<s>","<e>", "<UNK>")
-  
-  #########!!!!!!!!!!!!!!   This needs to clean target rather than just lower casing it
-  target<-tolower(target)
-  
-  
-  phrase.length <- stri_count_words(target) 
-  
-  if (phrase.length==1) target<-data.table(V1 = "", V2 = "", V3 = target) else 
-    if (phrase.length==2) target<-data.table(V1 = "", V2 = stri_extract_all_words(target)[[1]][1], V3 = stri_extract_all_words(target)[[1]][2]) else
-      target<-data.table(t(stri_extract_all_words(target)[[1]][(phrase.length-2):phrase.length]))
-  
-  #If phrase is longer than 3 words, discard all but the last 3
-  
-  
-  
-  setnames(target, c("V1","V2", "V3"),c("u","v","w"))
-  
-  if (phrase.length>3) {phrase.length<-3}
-  
-  
-  # replace out of vocab words in target with <UNK> tag
-  target[,lapply(.SD , function(y) ifelse ((y %in% unigrams$x), y , "<UNK>"))]
-  
-  if (model %in% c("Backoff")) {
-    
-    
-    # target<-data.table(u = target[1],v = target[2],w = target[3])
-    
-    if ( phrase.length  == 3 ) { y<-quadrigrams[target , .(x,probability), nomatch = 0 ]
-    
-    if (y[,.N]==0) phrase.length<-phrase.length-1 else y<-y[order(probability)[1:n],x]}#print("Backing off to trigrams")
-    
-    
-    if ( phrase.length  == 2 ) {y<-    trigrams[target[,.(v,w)] , .(x,probability)]
-    
-    
-    
-    if (y[,.N]==0) phrase.length<-phrase.length-1 else y<-y[order(probability)[1:n], w]} #print("Backing off to bigrams")
-    
-    
-    
-    
-    if ( phrase.length  == 1 ) {y<-     bigrams[target[,w], .(x,probability)]
-    
-    
-    if (y[,.N]==0) phrase.length<-phrase.length-1 else y<-y[order(probability)[1:n],v]} #print("Backing off to unigrams")
-    
-    
-    
-    if ( phrase.length  == 0 )  y<-    unigrams[order(probability),ngram ][1:n]
-    
-  }
-  
-  else if (model=="Interpolate"){
-    
-    #create a table of probabilites of words based on for quadrigrams -> unigrams
-    table.predict<-  data.table(rbind(
-      
-      
-      quadrigrams[target[,.(u,v,w)], ][!(x %in% tags)][,weighted.prob := l4* 2^-probability][order(-weighted.prob), .(x, weighted.prob)][1:min(.N,10)],   
-      trigrams   [target[,.(  v,w)], ][!(x %in% tags)][,weighted.prob := l3* 2^-probability][order(-weighted.prob), .(x, weighted.prob)][1:min(.N,10)], 
-      bigrams    [target[,.(    w)], ][!(x %in% tags)][,weighted.prob := l2* 2^-probability][order(-weighted.prob), .(x, weighted.prob)][1:min(.N,10)], 
-      unigrams                        [!(x %in% tags)][,weighted.prob := l1* 2^-probability][order(-weighted.prob), .(x, weighted.prob)]))
-    
-    
-    
-    #sum the probabilities      
-    y<-table.predict[, sum(weighted.prob, rm.na = T), by = x][order(-V1), x][1:n]
-    
-    
-    
-  }
-  
-  else print("Not a valid model")
-  y}
 
 
 shinyApp(ui = ui, server = server)
